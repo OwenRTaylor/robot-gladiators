d3f56cb60d68c17f09139b4e3009513bd5ff87f1
Ortaylo <99422408+Ortaylo@users.noreply.github.com>
2022-03-01 17:27:55 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,12 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+var playerMoney = 10;
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  if (promptFight === "fight" || promptFight === "FIGHT"){
   enemyHealth -= playerAttack;
   playerHealth -= enemyAttack;
   console.log(
@@ -34,6 +37,19 @@ if (playerHealth <= 0) {
   else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+
+
+ } else if (promptFight === "skip" || promptFight === "SKIP") {
+     var confirmSkip = window.confirm("are you sure you's like to quit?")
+
+     if (confirmSkip) {
+         window.alert(playerName + " has decided to skip this fight. Goodbye!");
+         playerMoney = playerMoney - 2
+     }
+ } else {
+     window.alert(" You need to choose a valid option. Try again!");
+ }
+ 
 };
 
 fight();
